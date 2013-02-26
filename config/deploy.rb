6c7d079ed37c040f2fc9d8cc47e8639a58057206
Tyler Davis <tyebud@gmail.com>
2013-02-26 16:05:01 -0500

    Capify

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+set :application, "ScrapeWeb"
+set :repository,  "git@github.com:tylerdavis/scrape_web.git"
+
+set :user, 'tyler'
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
+
+set :scm, :git
+
+default_run_options[:pty] = true
+
+role :web, "96.8.123.70"
+role :app, "96.8.123.70"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
